fd9592d738dc0e09f327b3ad345113e37264f4e5
FOCKUSTY <wwddwwwd@mail.ru>
2026-02-03 21:32:11 +0500

    -up

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,24 +1,76 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, useTemplateRef } from "vue";
 const count = ref<number>(0);
+const pivo = useTemplateRef("pivo");
+const pivoEmpty = useTemplateRef("pivoEmpty");
+const text = ref<string>(`Нажато: ${count.value}`);
+
+const random = () => {
+  return Math.round(Math.random() * 100);
+};
 
 const increment = () => {
   count.value++;
+  text.value = `Нажато: ${count.value}`;
+};
+
+const drinkPivo = () => {
+  if (!pivo.value || !pivoEmpty.value) {
+    return;
+  }
+
+  if (random() > 3) {
+    return;
+  }
+
+  pivo.value.style.height = "0px";
+  pivo.value.style.opacity = "0";
+  pivoEmpty.value.style.display = "";
+  pivoEmpty.value.style.opacity = "1";
+
+  text.value = "Вы случайно выпили пиво! Сейчас нальём новое!";
+
+  setTimeout(() => {
+    if (!pivo.value || !pivoEmpty.value) {
+      return;
+    }
+
+    pivoEmpty.value.style.display = "none";
+    pivoEmpty.value.style.opacity = "0";
+    pivo.value.style.height = "";
+    pivo.value.style.opacity = "";
+
+    text.value = "Нажато: " + count.value;
+  }, 2000);
 };
 </script>
 
 <template>
   <section class="flex flex-col h-full w-full items-center justify-center">
-    <div class="flex items-center justify-center h-48 aspect-cubic">
+    <div class="flex items-end justify-center h-48 aspect-cubic">
+      <img
+        src="/pivo-empty.svg"
+        alt="pivo"
+        ref="pivoEmpty"
+        class="absolute h-48 transition-all duration-200 aspect-cubic noselect"
+        style="opacity: 0"
+      />
+
       <img
         src="/pivo.svg"
         alt="pivo"
-        class="h-48 active:h-40 transition-all duration-200 aspect-cubic cursor-pointer noselect"
-        @click="increment"
+        ref="pivo"
+        class="z-1 h-48 active:h-40 transition-all duration-200 cursor-pointer noselect"
+        @click="
+          () => {
+            increment();
+            drinkPivo();
+          }
+        "
       />
     </div>
 
-    <span>Нажато: {{ count }}</span>
+    <span>{{ text }}</span>
   </section>
 
   <div class="flex flex-col text-mini">
